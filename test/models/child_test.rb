ad1c8976680fd75060f54326059158fdb5aefea7
multikasia <kasia.ekiert@gmail.com>
2023-01-26 15:04:48 +0100

    Progress in model test Child

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -8,23 +8,23 @@ class ChildTest < ActiveSupport::TestCase
 
   #add test to child model  
 
-  test "child should have born date" do
-    child = Child.new(born_at: "")
+  test "child should have name and born date" do
+    child = Child.new(name: "Emma", born_at: "")
     assert_not child.valid?
   end
 
   test "child should be born today" do
-    child = Child.new(born_at: Date.today)
+    child = Child.new(name: "Emma", born_at: Date.today)
     assert child.valid?
   end
 
   test "child should be born in the past" do
-    child = Child.new(born_at: Date.today - 2.days)
+    child = Child.new(name: "John", born_at: Date.today - 2.days)
     assert child.valid?
   end
 
   test "child should not be born in the future" do
-    child = Child.new(born_at: Date.today + 2.days)
+    child = Child.new(name: "John", born_at: Date.today + 2.days)
     assert_not child.valid?
   end
 end

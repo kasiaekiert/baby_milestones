ea05132ee7c7531949ca2f61eb8685ce59755c93
multikasia <kasia.ekiert@gmail.com>
2023-01-26 15:00:39 +0100

    Tests for model Child

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -2,7 +2,29 @@ require "test_helper"
 
 class ChildTest < ActiveSupport::TestCase
   test "child should have name" do
-    child = Child.new(name: "")
+    child = Child.new(name: "", born_at: 2.days.ago)
+    assert_not child.valid?
+  end
+
+  #add test to child model  
+
+  test "child should have born date" do
+    child = Child.new(born_at: "")
+    assert_not child.valid?
+  end
+
+  test "child should be born today" do
+    child = Child.new(born_at: Date.today)
+    assert child.valid?
+  end
+
+  test "child should be born in the past" do
+    child = Child.new(born_at: Date.today - 2.days)
+    assert child.valid?
+  end
+
+  test "child should not be born in the future" do
+    child = Child.new(born_at: Date.today + 2.days)
     assert_not child.valid?
   end
 end

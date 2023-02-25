f5ba7ae7fba6d657a99bffa20d9a99afb57ddd3d
Iris de la Luna <iris@random.town>
2023-02-25 11:26:15 +0100

    Add logs to reloading child assignments

--- app/models/child.rb
+++ app/models/child.rb
@@ -7,10 +7,12 @@ class Child < ApplicationRecord
   validates_date :born_at, on_or_before: lambda { Date.today }
   
   def reload_assignments!
+    logger.info("Reloading assignments for #{child.name.inspect} (#{child.id})")
     query = Milestone.where("breakpoint_day <= ?", age_in_days)
     last_milestone = milestones.order(breakpoint_day: :desc).first
     query = query.where("breakpoint_day > ?", last_milestone.breakpoint_day) if last_milestone
     query.each do |milestone|
+      logger.info("Assigning #{milestone.title.inspect} (#{milestone.id}) to #{child.name.inspect} (#{child.id})")
       assignments.build(milestone: milestone)
     end
     save!
